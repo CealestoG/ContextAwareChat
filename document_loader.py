8705b3cd8f94cf2044fbbbdaf35d00d745acf39d
Sarkar <159285447+CealestoG@users.noreply.github.com>
2025-07-23 18:41:29 +0530

    Update document_loader.py
    added comments for better understanding

--- document_loader.py
+++ document_loader.py
@@ -2,14 +2,15 @@ import os
 from langchain_community.document_loaders import DirectoryLoader, TextLoader 
 
 def load_documents(path):
+    # Check if the path is a single file
     if os.path.isfile(path):
-        
         loader = TextLoader(path)
+    
+    # Check if the path is a directory containing multiple text files
     elif os.path.isdir(path):
-        
         loader = DirectoryLoader(
             path=path,
-            glob="**/*.txt",
+            glob="**/*.txt",  # Match all .txt files recursively
             loader_cls=TextLoader
         )
     else:
